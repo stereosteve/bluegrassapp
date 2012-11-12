8b89f2170d55cddd76052d4d326520937314b842
Steve Perkins <steve@indabamusic.com>
2012-11-12 12:28:12 -0500

    Ingest google spreadsheet + build song db

--- ingest.js
+++ ingest.js
@@ -1,13 +1,43 @@
-var agent = require('superagent');
-var csv = require('csv');
+var agent = require('superagent')
+  , csv = require('csv')
+  , _ = require('underscore')
 
-agent
-  .get('https://docs.google.com/spreadsheet/pub?key=0AlzWAe7sSRCrdFYzWEJfWThCRDBoMXNBczdLQm9PS3c&output=csv')
-  .end(function(resp) {
+
+var ingest = module.exports = function(cb) {
+  var fields = []
+  var data = {}
+
+  function onResponse(text) {
     csv()
-      .from(resp.text)
-      .on('record', function(data, index) {
-        console.log("\n\n-------------", index, '\n', data)
-        console.log(data[2])
+      .from(text)
+      .on('record', onRecord)
+      .on('end', function() {
+        cb(data)
       })
+  }
+
+  function onRecord(row, index) {
+    if (index === 0) {
+      fields = row
+      return
+    }
+    var song = _.object(fields, row)
+    song.id = index
+    song.tags = song.tags.split('\n')
+    data[song.id] = song
+  }
+
+  agent
+    .get('https://docs.google.com/spreadsheet/pub?key=0AlzWAe7sSRCrdFYzWEJfWThCRDBoMXNBczdLQm9PS3c&output=csv')
+    .end(function(resp) {
+      onResponse(resp.text)
+    })
+}
+
+
+// test
+if (!module.parent) {
+  ingest(function(data) {
+    console.log("DATA", data)
   })
+}
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var express = require('express')
 
 var app = express();
 
+//
+// Config
+//
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -17,8 +20,6 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
   app.locals.info = info
 });
-
-
 app.configure('development', function(){
   app.locals.manifest = ''
   //app.locals.manifest = 'manifest="/offline.appcache"'
@@ -28,6 +29,9 @@ app.configure('production', function() {
   app.locals.manifest = 'manifest="/offline.appcache"'
 })
 
+//
+// Mobile app
+//
 app.get('/', function(req, resp) {
   var v = req.query.v
   if (v !== info.version) {
@@ -41,6 +45,23 @@ app.get('/offline.appcache', function(req, resp) {
   resp.set('Content-Type', 'text/cache-manifest');
   resp.render('offline')
 });
+
+
+//
+// Spreadsheet DB
+//
+var songDb;
+app.get('/songs', function(req, resp) {
+  resp.json(songDb)
+})
+app.get('/songs/:id', function(req, resp) {
+  resp.json(songDb[req.params.id])
+})
+
+var ingest = require('./ingest')
+ingest(function(data) {
+  songDb = data
+})
 
 
 http.createServer(app).listen(app.get('port'), function(){
